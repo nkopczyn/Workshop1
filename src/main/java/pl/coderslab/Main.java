10effb91297e252203218e647ee9c46c79eff56e
“nkopczyn” <nkopczyn@visa.com>
2025-06-26 16:07:43 +0200

    commit test 2

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
-        System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
 
-            System.out.println("i = " + i);
+        System.out.printf("test");
+
+
         }
     }
-}
